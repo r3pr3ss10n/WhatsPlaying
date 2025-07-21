2d2f5bac79219a540d2fab3303dbbf36f8edecad
r3pr3ss10n <qqqeeetttuuuoooj@gmail.com>
2025-07-22 00:37:03 +0300

    Initial Commit

---
+++ WhatsPlaying/WhatsPlayingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsPlayingApp.swift
+//  WhatsPlaying
+//
+//  Created by key on 22.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsPlayingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
